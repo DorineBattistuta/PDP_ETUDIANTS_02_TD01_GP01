52680136ee95ccc48c6208311f89b13a76f3b70a
DorineBattistuta <dorine.battistuta@edu.ece.fr>
2024-02-05 12:52:12 +0100

    etape_2

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,54 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+//...
+
+
 
 // Define the pins that we will use
 #define SENSOR 33
 #define LED 26
 #define DHTTYPE DHT11
 
+#define BLYNK_TEMPLATE_ID "TMPL56R0NuV8l"
+#define BLYNK_TEMPLATE_NAME "TP02"
+#define BLYNK_AUTH_TOKEN "ppZyC0YW1SH4znyvadEm5c2wKyhyNUlq"
+
+// EN DESSOUS DES DEFINES POUR BLYNK_TEMPLATE ID, ETC
+#include <BlynkSimpleEsp32.h>
+
+#define BLYNK_PRINT Serial
+
+
 DHT_Unified dht(SENSOR, DHTTYPE);
 
-// WiFi credentials go here
-// ...
-// ...
-// ...
+// Avant le setup
+char ssid[] = "HelloWorld";
+char pass[] = "testtest";
+
+BLYNK_WRITE(V2)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V0 to a variable
+  Serial.print("Received value from Blynk: ");
+  Serial.println(pinValue);
+  digitalWrite(LED,pinValue);
+  // Delay is only there so that we get a chance to see the LED value properly.
+  delay(1000);
+}
 
 void setup() {
+
+ 
   // Setup pins
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
+
+   // Au début du setup, après la connexion série
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  Blynk.run();
 
   // Begin serial communication
   Serial.begin(9600);
@@ -46,6 +77,11 @@ void setup() {
     temp_measure = event.temperature;
   }
 
+  // Send temperature data to Blynk
+    Blynk.virtualWrite(V1, temp_measure); // Assuming virtual pin V1 is used for temperature
+  
+  Blynk.syncVirtual(V2);
+
   // Get humidity event and print its value.
   float relative_humidity_measure = -999.0;
   dht.humidity().getEvent(&event);
@@ -58,10 +94,9 @@ void setup() {
     relative_humidity_measure = event.relative_humidity;
   }
 
-  // Send data to Blynk
-  // ...
-  // ...
-  // ...
+  // Send humidity data to Blynk
+    Blynk.virtualWrite(V0, relative_humidity_measure); // Assuming virtual pin V2 is used for humidity
+  
 
   Serial.println("Going to sleep for 5 seconds...");
   delay(100);
